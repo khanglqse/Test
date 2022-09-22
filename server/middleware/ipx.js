6ec1de8eabbe7870770268db6ce657136ec5e67f
Khang Luong <khanglqse@gmail.com>
2022-09-22 10:34:15 +0700

    update ipx

--- server/middleware/ipx.js
+++ server/middleware/ipx.js
@@ -1,7 +1,9 @@
-import { createIPX, createIPXMiddleware } from "ipx";
-
-export default createIPXMiddleware(
-    createIPX({
-        domains: ["https://cdn.aXXXXXXh.ir"],
-    })
-);
+import { createIPX, createIPXMiddleware } from 'ipx'
+// https://github.com/unjs/ipx
+const ipx = createIPX({
+  dir: '', // absolute path to images dir
+  domains: [], // allowed external domains (should match domains option in nuxt.config)
+  alias: {}, // base alias
+  sharp: {}, // sharp options
+})
+export default createIPXMiddleware(ipx)
