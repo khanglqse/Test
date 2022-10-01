df3660493f143a8ad0fa8a8227e8a5f7c224965e
anonymous <anonymous>
2022-10-02 00:20:32 +0700

    update code landing page

--- components/Header.vue
+++ components/Header.vue
@@ -30,17 +30,6 @@
               <div class="mainmenu">
                 <div class="gc_right_menu">
                   <ul>
-                    <!-- <li id="search_button">
-                      <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"
-                        id="Capa_3" x="0px" y="0px" viewBox="0 0 451 451" style="enable-background: new 0 0 451 451"
-                        xml:space="preserve">
-                        <g>
-                          <path id="search"
-                            d="M447.05,428l-109.6-109.6c29.4-33.8,47.2-77.9,47.2-126.1C384.65,86.2,298.35,0,192.35,0C86.25,0,0.05,86.3,0.05,192.3   s86.3,192.3,192.3,192.3c48.2,0,92.3-17.8,126.1-47.2L428.05,447c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4   C452.25,441.8,452.25,433.2,447.05,428z M26.95,192.3c0-91.2,74.2-165.3,165.3-165.3c91.2,0,165.3,74.2,165.3,165.3   s-74.1,165.4-165.3,165.4C101.15,357.7,26.95,283.5,26.95,192.3z"
-                            fill="#23c0e9" />
-                        </g>
-                      </svg>
-                    </li> -->
                     <li>
                       <div id="search_open" class="gc_search_box">
                         <input type="text" placeholder="Search here" />
@@ -64,11 +53,6 @@
                         <!-- <a href="index-2.html">Home1</a> -->
                         <nuxt-link to="/"> Publisher </nuxt-link>
                       </li>
-                      <!-- <li class="parent"><a href="index_II.html">Home2</a></li>
-                      <li class="parent"><a href="index_map.html">Home3</a></li>
-                      <li class="parent"><a href="index_iv.html">Home4</a></li>
-                      <li class="parent"><a href="index_v.html">Home5</a></li>
-                      <li class="parent"><a href="index_vi.html">Home6</a></li> -->
                     </ul>
                   </li>
                   <li class="has-mega gc_main_navigation">
